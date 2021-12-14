839f4eda5d36a164d7dcad6bfbd99a383a1f4ec2
Ritik Wadhwani <ritik.wadhwani55@gmail.com>
2021-12-14 09:10:30 +0530

    Initial Setup

---
+++ server.js
@@ -0,0 +1,21 @@
+if(process.env.NODE_ENV!=='production'){
+    require('dotenv').config()
+}
+const express= require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require('./routes/index.js')
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (e) => console.error(e));
+db.once('open', ()=> console.log('Connected to mongodb'));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname+'/views');
+app.set('layout', 'layouts/layout');
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use('/', indexRouter);
+app.listen(process.env.PORT || 3000)
